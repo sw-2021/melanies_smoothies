c6fb5e81e663487eecf31e3598199e20c109e3f4
sw-2021 <77554723+sw-2021@users.noreply.github.com>
2025-12-29 08:03:27 +0000

    Update streamlit_app.py
    Pull back individual ingredients from API

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx=st.connection("snowflake")
 session=cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, width='stretch')
 
 ingredients_list=st.multiselect("Choose up to 5 ingredients:",my_dataframe,max_selections=5)
 
@@ -30,9 +30,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         # st.text(smoothiefroot_response.json())
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),width='stretch')
 
     # st.write(ingredients_string)
     
